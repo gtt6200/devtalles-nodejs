a59dd67d67c5af576a67dae181258dda6743cc68
gtt6200 <gtt6200@gmail.com>
2024-11-22 16:24:28 -0600

    fix merge branch to main

--- 05-NOC/src/presentation/cron/cron-service.ts
+++ 05-NOC/src/presentation/cron/cron-service.ts
@@ -3,13 +3,13 @@ import { CronJob } from "cron";
 type CronTime = string | Date;
 type OnTick = () => void;
 
-export class CronService{
+export class CronService {
 
-    public static createJob( cronTime: CronTime, onTick: OnTick): CronJob {
+    public static createJob(cronTime: CronTime, onTick: OnTick): CronJob {
         console.log("Cron job started... from cron-service.ts");
-        
+
         const job = new CronJob(cronTime, onTick);
-        
+
         job.start();
 
         return job;
